83b673d450aa04a85d8a2f7ce00077c53636bdfb
specialblend <cj@specialblend.net>
2020-06-30 00:51:33 -0500

    resolve exception code from classname

--- src/Exception.ts
+++ src/Exception.ts
@@ -1,28 +1,18 @@
-interface Serializable {
-    toString: () => string
-}
-
-export interface ExceptionMetadata {
-    message: string,
-    code: string,
-}
-
-export interface WrappedErrorExceptionPayload {
-    err: Error,
-}
-
 type Nullable<T> = T | null;
 
 export class Exception<PAYLOAD_TYPE, ERROR_TYPE> {
-    private metadata: ExceptionMetadata;
+    private _message: string;
+    private _err: Nullable<ERROR_TYPE>;
     private data: PAYLOAD_TYPE;
-    private err: Nullable<ERROR_TYPE>;
 
-    constructor(metadata: ExceptionMetadata, data: PAYLOAD_TYPE, err: Nullable<ERROR_TYPE> = null) {
-        const { message, code } = metadata;
-        this.metadata = { message, code };
+    constructor(message: string, data: PAYLOAD_TYPE, err: Nullable<ERROR_TYPE> = null) {
+        this._message = message;
         this.data = { ...data };
-        this.err = err;
+        this._err = err;
+    }
+
+    public get code(): string {
+        return this.constructor.name;
     }
 
     /**
@@ -31,18 +21,10 @@ export class Exception<PAYLOAD_TYPE, ERROR_TYPE> {
      */
     public toError(): Error {
         const message = this.formatErrorMessage();
-        const { data, metadata } = this;
         const err = new Error(message);
-        Object.assign(err, { metadata, data });
+        const data = { ...this.data };
+        Object.assign(err, { data });
         return err;
-    }
-
-    /**
-     * Format the error message when mapping to a  JavaScript Error object
-     * @returns {string} error message
-     */
-    protected formatErrorMessage(): string {
-        return [this.metadata.code, this.metadata.message].join(': ');
     }
 
     /**
@@ -51,8 +33,16 @@ export class Exception<PAYLOAD_TYPE, ERROR_TYPE> {
      * @param {Function} ExceptionConstructor the new Exception constructor
      * @returns {Exception} the new Exception
      */
-    public as<EXCEPTION_TYPE extends Exception<PAYLOAD_TYPE, ERROR_TYPE>>(ExceptionConstructor: new <P, C>(metadata: ExceptionMetadata, data: PAYLOAD_TYPE) => EXCEPTION_TYPE): EXCEPTION_TYPE {
-        return new ExceptionConstructor<PAYLOAD_TYPE, CODE_TYPE>(this.metadata, this.data);
+    public as<EXCEPTION_TYPE extends Exception<PAYLOAD_TYPE, ERROR_TYPE>>(ExceptionConstructor: new <P, C>(message: string, data: PAYLOAD_TYPE) => EXCEPTION_TYPE): EXCEPTION_TYPE {
+        return new ExceptionConstructor<PAYLOAD_TYPE, CODE_TYPE>(this._message, this.data);
+    }
+
+    /**
+     * Format the error message when mapping to a  JavaScript Error object
+     * @returns {string} error message
+     */
+    protected formatErrorMessage(): string {
+        return [this.code, this._message].join(': ');
     }
 }
 
@@ -68,10 +58,9 @@ export type GenericException = Exception<Record<string, any>, Error>;
  * @param {*} data data
  * @returns {Exception} exception
  */
-export function fromError<PAYLOAD_TYPE, ERROR_TYPE extends Error>(err: ERROR_TYPE, code: string, data: PAYLOAD_TYPE): Exception<PAYLOAD_TYPE, ERROR_TYPE> {
+export function fromError<ERROR_TYPE extends Error, PAYLOAD_TYPE>(err: ERROR_TYPE, code: string, data: PAYLOAD_TYPE): Exception<PAYLOAD_TYPE, ERROR_TYPE> {
     const { message } = err;
-    const metadata = { message, code };
-    return new Exception<PAYLOAD_TYPE, ERROR_TYPE>(metadata, data, err);
+    return new Exception<PAYLOAD_TYPE, ERROR_TYPE>(message, data, err);
 }
 
 /**
