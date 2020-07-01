5715f2f753d1cb0daeb4356d8d4e314e74072103
specialblend <cj@specialblend.net>
2020-07-01 09:10:10 -0500

    wip

--- src/createExceptionWrapper.test.ts
+++ src/createExceptionWrapper.test.ts
@@ -9,32 +9,48 @@ const EX_AUTH_MISSING_TOKEN = Symbol('EX_AUTH_MISSING_TOKEN');
 const EX_AUTH_NOT_HAS_ABILITY = Symbol('EX_AUTH_NOT_HAS_ABILITY');
 const EX_AUTH_NOT_HAS_OWNERSHIP = Symbol('EX_AUTH_NOT_HAS_OWNERSHIP');
 
+const ExceptionMessageDictionary = {
+    [EX_AUTH_INVALID_CREDENTIALS]: 'The provided credentials are invalid. Please check your username and password, and try again.',
+    [EX_AUTH_MISSING_TOKEN]: 'The request is missing an authenticaton token. Please refresh the app and try again.',
+    [EX_AUTH_NOT_HAS_ABILITY]: 'The authenticated user does not have the ability to perform this action. Please contact your technical administration for further assistance.',
+    [EX_AUTH_NOT_HAS_OWNERSHIP]: 'The authenticated user does not have the require ownership over this resource to perform this action. Please contact your technical administration for further assistance.',
+};
+
 class InvalidCredentialsEx extends createExceptionWrapper(
     AuthenticationException,
-    'The provided credentials are invalid. Please check your username and password, and try again.',
+    ExceptionMessageDictionary[EX_AUTH_INVALID_CREDENTIALS],
     EX_AUTH_INVALID_CREDENTIALS
 ) {}
 
 class MissingTokenEx extends createExceptionWrapper(
     AuthenticationException,
-    'The request is missing an authenticaton token. Please refresh the app and try again.',
+    ExceptionMessageDictionary[EX_AUTH_MISSING_TOKEN],
     EX_AUTH_MISSING_TOKEN
 ) {}
 
 class NotHasAbilityEx extends createExceptionWrapper(
     AuthorizationException,
-    'The authenticated user does not have the ability to perform this action. Please contact your technical administration for further assistance.',
+    ExceptionMessageDictionary[EX_AUTH_NOT_HAS_ABILITY],
     EX_AUTH_NOT_HAS_ABILITY
 ) {}
+
 class NotHasOwnershipEx extends createExceptionWrapper(
     AuthorizationException,
-    'The provided credentials are invalid. Please check your username and password, and try again.',
+    ExceptionMessageDictionary[EX_AUTH_NOT_HAS_OWNERSHIP],
     EX_AUTH_NOT_HAS_OWNERSHIP
 ) {}
 
-describe('createExceptionWrapper', () => {
-    test('returns expected Exception', () => {
-        const err = new Error('I am ugly error.');
-        throw new InvalidCredentialsEx(err);
+describe('new InvalidCredentialsEx(Error)', () => {
+    const $err = new Error('I am ugly error.');
+    const $ex = new InvalidCredentialsEx($err);
+    test('inherits from expected classes', () => {
+        expect($ex).toBeInstanceOf(AuthenticationException);
+        expect($ex).toBeInstanceOf(AuthException);
+        expect($ex).toBeInstanceOf(Exception);
+    });
+    test('has expected properties', () => {
+        expect($ex.message).toBe(ExceptionMessageDictionary[EX_AUTH_INVALID_CREDENTIALS]);
+        expect($ex.code).toBe(EX_AUTH_INVALID_CREDENTIALS);
+        expect($ex.err).toBe($err);
     });
 });
